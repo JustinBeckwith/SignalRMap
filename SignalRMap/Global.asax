c5ee9737f98313885b4867e811975421f88ab103
Justin Beckwith <justin.beckwith@gmail.com>
2011-10-09 21:25:59 -0700

    initial commit

---
+++ SignalRMap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRMap.MvcApplication" Language="C#" %>
---
+++ SignalRMap/MyConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+using SignalR;
+
+namespace SignalRMap
+{
+	public class MyConnection : PersistentConnection
+	{
+
+		protected override Task OnReceivedAsync(string clientId, string data)
+		{
+			// Broadcast data to all clients
+			return Connection.Broadcast(data);
+		}
+	}
+}
